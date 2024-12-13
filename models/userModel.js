703a65b91e69c401475f71fcfcdeac9409bb2dd7
nalconnaughton <93225468+nalconnaughton@users.noreply.github.com>
2024-12-13 17:42:18 +0000

    CSP header added

--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,13 @@
 const db = require('../db');
 const bcrypt = require('bcrypt');
 
-//finds a users email
+//finds a users email, Parameterized Queries used
 const User = {
     findByEmail: (email, callback) => {
         db.query('SELECT * FROM users WHERE email = ?', [email], callback);
     },
 
-    //creates new user
+    //creates new user + hash added
     create: (username, email, hashedPassword, callback) => {
         db.query(
             'INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, hashedPassword], callback);
