66fc3f5c70f97b3b579e42dc0dc182db655e4cda
nalconnaughton <93225468+nalconnaughton@users.noreply.github.com>
2024-12-13 17:02:20 +0000

    SQL injection added
    code to be vulnerbale for sql injection added

--- models/userModel.js
+++ models/userModel.js
@@ -2,13 +2,16 @@ const db = require('../db');
 
 const User = {
     findByEmail: (email, callback) => {
-        db.query('SELECT * FROM users WHERE email = ?', [email], callback);
+        //Vulnerable to sql injection
+        //would log in as admin and bypass the authentication as anything after '--' will be ignored
+        const query = 'SELECT * FROM members WHERE username = 'admin'--' AND password = 'password';
+        db.query(query, callback);
     },
 
     create: (username, email, password, callback) => {
-        db.query(
-            'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
-            [username, email, password], callback);
+        // Vulnerable to SQL Injection as attacker can inject a payload
+        const query = 'INSERT INTO users (username, email, password) VALUES ('${username}', '${email}', '${password}')`;
+        db.query(query, callback);
     },
 };
 
