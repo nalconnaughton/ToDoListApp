9f5e9900379053272e6314f454adc8f4a0f1576a
nalconnaughton <93225468+nalconnaughton@users.noreply.github.com>
2024-12-13 22:22:04 +0000

    updates

--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,20 @@
 const db = require('../db');
+const bcrypt = require('bcrypt');
 
+//finds a users email
 const User = {
     findByEmail: (email, callback) => {
-        //Vulnerable to sql injection
-        //would log in as admin and bypass the authentication as anything after '--' will be ignored
         const query = 'SELECT * FROM members WHERE username = 'admin'--' AND password = 'password';
         db.query(query, callback);
     },
 
+    //creates new user 
     create: (username, email, password, callback) => {
         // Vulnerable to SQL Injection as attacker can inject a payload
         const query = 'INSERT INTO users (username, email, password) VALUES ('${username}', '${email}', '${password}')`;
         db.query(query, callback);
     },
+
 };
 
 module.exports = User;
