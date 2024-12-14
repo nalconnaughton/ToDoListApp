0a88139471fdee5fd439890e96105ed07b7921d8
nalconnaughton <93225468+nalconnaughton@users.noreply.github.com>
2024-12-14 18:18:08 +0000

    updates

--- models/userModel.js
+++ models/userModel.js
@@ -1,16 +1,16 @@
 const db = require('../db');
-const bcrypt = require('bcrypt');
 
-//finds a users email
+
+//finds a users email, Parameterized Queries used
 const User = {
     findByEmail: (email, callback) => {
         const query = 'SELECT * FROM members WHERE username = 'admin'--' AND password = 'password';
         db.query(query, callback);
+        
     },
 
-    //creates new user 
-    create: (username, email, password, callback) => {
-        // Vulnerable to SQL Injection as attacker can inject a payload
+    //creates new user + hash added
+    create: (username, email, hashedPassword, callback) => {
         const query = 'INSERT INTO users (username, email, password) VALUES ('${username}', '${email}', '${password}')`;
         db.query(query, callback);
     },
